03136b7c92b685fbb0a86844eac2600783ae5fa2
Elise Harman <elise_harman@hotmail.com>
2021-07-26 14:51:57 +0800

    Basic footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,43 @@
 import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="columns is-vcentered">
+          <div className="column has-text-centered">
+            <div className="buttons is-justify-content-center">
+              <a
+                className="button is-info"
+                href="mailto:elise_harman@hotmail.com"
+              >
+                Email
+              </a>
+              <a
+                className="button is-info"
+                href="https://www.linkedin.com/in/elise-harman-1676b4a8/"
+                target="blank"
+              >
+                Linkedin
+              </a>
+              <a
+                className="button is-info"
+                href="https://github.com/harmane4"
+                target="blank"
+              >
+                GitHub
+              </a>
+              <a
+                className="button is-info"
+                href="Assests/images/ResumeEliseHarman.pdf"
+                target="blank"
+              >
+                Resume
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
